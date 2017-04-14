e638340855b875f43992d4fb04139e6534641697
khrystyan88 <khrystyan881@hotmail.com>
2017-04-14 05:46:00 -0500

    añadido bootrap
    mejorando la interfaz

--- index.php
+++ index.php
@@ -1,3 +1,58 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+	<title>Problemas PHP</title>
+</head>
+<body>
+
+<div class="container">
+	
+
+
+	<h1> Problemas Resueltos for @Erick Cesar Salazar Enriquez</h1>
+	
+	<div class="row">
+		<div class="col-md-4"></div>
+		<div class="col-md-4">
+	
+					<div class="list-group">
+									  <a href="#" class="list-group-item active ">
+									    Ejercicios 
+									  </a>
+									  <a href="#" class="list-group-item">Ejecicio	1</a>
+									  <a href="#" class="list-group-item">Ejecicio	1</a>
+									  <a href="#" class="list-group-item">Ejecicio	1</a>
+									  <a href="#" class="list-group-item">Ejecicio	1</a>
+					</div>
+		
+			</div>
+
+
+		</div>
+
+		<div class="row">
+				<div class="col-md-8">
+
+					<div class="list-group">
+						  <p> Hola esto fue para practicar un rato aprovechando que estoy de vacaciones y para no oxidar mi cerebro espero te sirvan y los compartas  estan alojados en github ....by @khrystyan88</p>
+					</div>
+				</div>	
+
+		</div>
+
+	</div>		
+<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+	  <script src="js/bootrap.bootstrap.min.js"></script>
+
+</body>
+</html>
+
+
 <?php 
 echo "hola mundo";
  ?>
